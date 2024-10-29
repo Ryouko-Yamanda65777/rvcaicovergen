44c510eae9a8a1825cfb0267cabe0410e4c93f5f
Ryouko <153080407+Ryouko-Yamanda65777@users.noreply.github.com>
2024-10-29 18:53:45 +0700

    Update and rename download_models.py to download-models.py

--- aicovergen/download-models.py
+++ aicovergen/download-models.py
@@ -1,23 +1,25 @@
+# download_models.py
 from pathlib import Path
 import requests
 
 MDX_DOWNLOAD_LINK = 'https://github.com/TRvlvr/model_repo/releases/download/all_public_uvr_models/'
 RVC_DOWNLOAD_LINK = 'https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/'
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 mdxnet_models_dir = BASE_DIR / 'mdxnet_models'
-rvc_models_dir = BASE_DIR 
+rvc_models_dir = BASE_DIR
 
+mdxnet_models_dir.mkdir(exist_ok=True)
+rvc_models_dir.mkdir(exist_ok=True)
 
 def dl_model(link, model_name, dir_name):
-    with requests.get(f'{link}{model_name}') as r:
+    with requests.get(f'{link}{model_name}', stream=True) as r:
         r.raise_for_status()
         with open(dir_name / model_name, 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
 
-
-if __name__ == '__main__':
+def main():
     mdx_model_names = ['UVR-MDX-NET-Voc_FT.onnx', 'UVR_MDXNET_KARA_2.onnx', 'Reverb_HQ_By_FoxJoy.onnx']
     for model in mdx_model_names:
         print(f'Downloading {model}...')
